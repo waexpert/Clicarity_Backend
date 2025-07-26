23d9371174ec6e7b440bb9bd71f93fab28539450
waexpert <contact@wa.expert>
2025-07-26 12:19:47 +0530

    edited ssl parmater to rejectedUnauthorized false

--- database/databaseConnection.js
+++ database/databaseConnection.js
@@ -6,9 +6,9 @@ const pool = new Pool({
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
-  //   ssl: {
-  //    rejectUnauthorized: false // For development - use proper SSL validation in production
-  //  }
+    ssl: {
+     rejectUnauthorized: false // For development - use proper SSL validation in production
+   }
 });
 
 
